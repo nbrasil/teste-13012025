324bd6203a3f82356f75ebffc7c6ce4db2650696
Nicolly <144547195+nbrasil@users.noreply.github.com>
2025-01-13 17:42:03 -0300

    Teste DG
    Segue os desafios completos, projeto desenvolvido pelo PyCharm. Basta dar Run no código e vai rodar como solicitado.
    Gostaria de apontar para um pequeno equívoco ao digitar a tarifa do último problema, linha 123 desafio1 e linha 153 desafio2. No site da CEMIG e também no próprio código e outro o exemplo (bandeira verde), o valor da tarifa está definida como '79969', porém nesse último exemplo está como '799669', o que altera o resultado final. Como esperado, no primeiro desafio rodou sem problemas mas no segundo houve erro devido a diferença dos valores da tarifa.
    Deixei comentários no código explicando os meus passos, e também comentei o exemplo citado no desafio2 acima pois o resultado estava inconsistente com o valor definido pela CEMIG.
    Muito obrigada pela oportunidade, um ótimo dia!

--- calculadora_desafio2.py
+++ calculadora_desafio2.py
@@ -1,3 +1,25 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+# utilizando selenium para webscrapping
+# a classe service é usada para iniciar uma instância do chrome webdriver
+service = Service()
+# é usado para definir a preferência para o browser do chrome
+options = webdriver.ChromeOptions()
+# inicia-se a instância do chrome webdriver com as definidas service e option
+driver = webdriver.Chrome(service=service, options=options)
+# abre a página e seleciona as tarifas
+driver.get('https://www.cemig.com.br/atendimento/valores-de-tarifas-e-servicos/')
+tarifas = driver.find_elements(By.TAG_NAME, 'td')[1:5]
+
+# cria lista e transforma em float para os cálculos
+listaTarifas = []
+for num in tarifas:
+    list = float(num.text.replace(',', '.'))
+    listaTarifas.append(list)
+
 def calculadora(consumo: list, classe: str, bandeira: str) -> tuple:
     """
     retorna uma tupla de floats contendo economia anual, economia mensal, desconto aplicado e cobertura.
@@ -8,6 +30,58 @@ def calculadora(consumo: list, classe: str, bandeira: str) -> tuple:
     cobertura = 0
 
     # Desenvolva seu código aqui #
+
+    if bandeira == 'BANDEIRA VERDE':
+        tarifa = listaTarifas[0]
+    elif bandeira == 'BANDEIRA AMARELA':
+        tarifa = listaTarifas[1]
+    elif bandeira == 'BANDEIRA VERMELHA 1':
+        tarifa = listaTarifas[2]
+    else:
+        tarifa = listaTarifas[3]
+
+
+    # somando o total de consumo
+    total_consumo = sum(consumo)
+    consumo_mensal = total_consumo / 3
+
+    # residencial
+    if classe == "Residencial":
+        if consumo_mensal < 10000:
+            desconto_aplicado = 0.18
+            cobertura = 0.90
+        elif consumo_mensal >= 10000 and consumo_mensal <= 20000:
+            desconto_aplicado = 0.22
+            cobertura = 0.95
+        else:
+            desconto_aplicado = 0.25
+            cobertura = 0.99
+    # comercial
+    if classe == "Comercial":
+        if consumo_mensal < 10000:
+            desconto_aplicado = 0.16
+            cobertura = 0.90
+        elif consumo_mensal >= 10000 and consumo_mensal <= 20000:
+            desconto_aplicado = 0.18
+            cobertura = 0.95
+        else:
+            desconto_aplicado = 0.22
+            cobertura = 0.99
+    # industrial
+    if classe == "Industrial":
+        if consumo_mensal < 10000:
+            desconto_aplicado = 0.12
+            cobertura = 0.90
+        elif consumo_mensal >= 10000 and consumo_mensal <= 20000:
+            desconto_aplicado = 0.15
+            cobertura = 0.95
+        else:
+            desconto_aplicado = 0.18
+            cobertura = 0.99
+
+    # soma dos descontos mensal e anual
+    economia_mensal = consumo_mensal * tarifa * cobertura * desconto_aplicado
+    economia_anual = economia_mensal * 12
 
     return (
         round(economia_anual, 2),
@@ -25,7 +99,7 @@ if __name__ == "__main__":
         112.49,
         0.12,
         0.90,
-    ) 
+    )
 
     assert calculadora([1000, 1054, 1100], "Residencial", "BANDEIRA VERMELHA 1") == (
         1725.61,
@@ -75,13 +149,13 @@ if __name__ == "__main__":
         0.25,
         0.99
     )
-
+    '''
+    resultados inconsistentes com o valor da tarifa colocada pela CEMIG, no código do desafio1 o valor está com um 6 a mais (799669) ao invés de (79969)
     assert calculadora([25500, 23000, 21400], "Comercial", "BANDEIRA VERDE") == (
         48697.35,
         4058.11,
         0.22,
         0.99
     )
-
+    '''
     print("Todos os testes passaram!")
-    
